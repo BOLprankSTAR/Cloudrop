e683e7fe233eac6524307b3fa10161c7742c93ba
BOLprankSTAR <marco.alexander.schultheis@gmail.com>
2015-10-09 21:44:55 +0200

    [2.0.3] Ward Jump
    Changed repository index

---
+++ Utility/Ward Jump/translation table.lua
@@ -0,0 +1,67 @@
+--[[
+	translation table
+	example -> 		Language:Sprache
+					hello:hallo
+					translated by yourname:Übersetzt von prankSTAR
+
+	--------------------------------------------------
+
+	Language in your Language (e.g. Deutsch, English): 
+	Ward Jump:
+	general settings:
+	Key Binding:
+		Key Binding:
+		Ward Jump key settings:
+		quotequotequote:
+	Graphics:
+		general:
+		Enable all graphics:
+		ward jump:
+		Jump range:
+		Ward position:
+		Unit scan:
+		Jump information:
+		quotequotequote
+	ward jump settings:
+	Ward Jump:
+		unit scan:
+		Adjust scan radius:
+		quotequotequote:
+	Priorities:
+		wards:
+		Use Stealth Ward:
+		Set priority:
+		Use Vision Ward:
+		Set priority:
+		trinkets:
+		Use Warding Totem:
+		Set priority:
+		Use Greater Totem:
+		Set priority:
+		Use Greater Stealth Totem:
+		Set priority:
+		Use Greater Vision TOtem
+		Set priority:
+		others:
+		Use (Ruby) Sightstone
+		Set priority:
+		quotequotequote:
+	other settings:
+	Language:
+		language:
+		Choose a language:
+		translated by prankSTAR:
+		quotequotequote:
+	MenuConfig:
+		gamestart:
+		Activate message on GameStart:
+		settings storage:
+		Reset settings:
+		quotequotequote:
+	about the script:
+	Version:
+	Author:
+	Updated:
+	Optimized:
+	next generation script:
+--]]
